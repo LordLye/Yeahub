100e425fcb521404db44e7a1ea03c9007612e3ab
Lye <lye3@mail.ru>
2026-09-14 20:06:29 +0300

    fix: force git to update folder case to lowercase

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
